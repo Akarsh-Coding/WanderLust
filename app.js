2a3233b8a4f4c0f9575b73d9195198d917e24dc7
Akarsh-Coding <akarsh4.pvt@gmail.com>
2026-03-04 23:49:36 +0530

    feat: upgrade to cloud database using MongoDB Atlas and implement persistent session storage with connect-mongo

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -22,7 +23,11 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 
-const Mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "0.0.0.0"]);
+
+// const Mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 
 main()
@@ -33,7 +38,7 @@ main()
     });
 
 async function main() {
-    await mongoose.connect(Mongo_url);
+    await mongoose.connect(dbUrl);
 }
 
 
@@ -45,8 +50,21 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24*3600,
+})
+
+store.on("error", (err)=> {
+    console.log("Error in Mongo Session Store", err)
+});
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store: store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
