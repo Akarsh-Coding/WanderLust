3a08a879bd9fc84f59a3f5ee8848ab0d0d973fcf
Akarsh-Coding <akarsh4.pvt@gmail.com>
2026-03-05 07:19:45 +0530

    feat: configure init script to upload sample listings data to MongoDB Atlas

--- init/index.js
+++ init/index.js
@@ -1,8 +1,17 @@
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "0.0.0.0"]);
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
 
 main()
   .then(() => {
@@ -13,14 +22,14 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
+  await initDB();
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data=initData.data.map((obj)=>({...obj, owner:"699bee84dcc24ad530d42a4f"}));
+  initData.data=initData.data.map((obj)=>({...obj, owner:"69a8714e215aefea86143805"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
 
-initDB();
